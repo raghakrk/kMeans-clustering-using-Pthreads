c9c7ed5c54afb98938c826d311f186a6fe9178df
Raghak Radhakrishnan <raghakrk7@gmail.com>
2019-10-26 23:24:49 -0700

    Update kmean-serial.cpp

--- kmean-serial.cpp
+++ kmean-serial.cpp
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <math.h>
 #include<iostream>
-#include <omp.h>
 
 #define h  800
 #define w  800
 
 #define input_file  "input.raw"
 #define output_file "output.raw"
+unsigned char* a = (unsigned char*)malloc(sizeof(unsigned char) * h * w);
+unsigned char* b = (unsigned char*)calloc(h * w,sizeof(unsigned char));
+
 using namespace std;
-float means[4] = { 0,85,170,255 };
+float means[6] = { 0,65,100,125,190,255};
+float csum[6]={0,0,0,0,0,0};
+float cnt[6]={0,0,0,0,0,0};
+int clustval[6]={0,51,102,153,204,255};
 
 int classify(int val)
 {
 	int min = INT8_MAX;
 	int index = -1;
 	float dist;
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		dist = sqrt(pow(val- means[i],2));
 		if (dist < min)
@@ -25,8 +30,11 @@ int classify(int val)
 			min = dist;
 			index = i;
 		}
+		
 	}
-return index;
+	csum[index]+=val;
+	cnt[index]+=1;
+	return index;
 }
 
 
@@ -36,7 +44,7 @@ int main(int argc, char** argv) {
 	int i;
 	FILE* fp;
 
-	unsigned char* a = (unsigned char*)malloc(sizeof(unsigned char) * h * w);
+	
 
 	// the matrix is stored in a linear array in row major fashion
 	if (!(fp = fopen(input_file, "rb"))) {
@@ -49,37 +57,39 @@ int main(int argc, char** argv) {
 	// measure the start time here
 	if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) { perror( "clock gettime" );}
 
-	//  Your code goes here
-	int iterations = 0; int val; int index; int clusterSize[4];
-	unsigned char* b = (unsigned char*)calloc(h * w,sizeof(unsigned char));
-	while (iterations<30)
+	// main code goes here
+	int iterations = 0; int val; int index;
+
+	while (iterations<50)
 	{
 		for (i = 0; i < 800 * 800; i++)
 		{
 			val = double(a[i]);
 			index = classify(val);
-			b[i] = index;
+			
 		}
-		for (int k = 0; k < 4; k++)
+		for (int k = 0; k < 6; k++)
 		{
-			int temp = 0;
-			int cnt = 0;
-			for (i = 0; i < 800 * 800; i++)
-			{
-				if (b[i] == k)
-				{
-					b[i] = 64 * k + 63;
-					temp += b[i];
-					cnt += 1;
-				}
-			}
-			means[k] = temp / cnt;
-			clusterSize[k] = cnt;
+			means[k] = csum[k] / cnt[k];
 		}
-
 
 		iterations += 1;
 
+	}
+	for (int i=0;i<800*800;i++)
+	{
+		float dist;
+		int minv = INT8_MAX;
+		int index=-1;
+		for (int j=0;j<6;j++)
+		{	dist = abs((a[i]- means[j]));
+			if (dist < minv)
+			{
+				minv = dist;
+				index = j;
+			}
+		}
+		b[i]=clustval[index];
 	}
 	// measure the end time here
 	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror( "clock gettime" );}		
